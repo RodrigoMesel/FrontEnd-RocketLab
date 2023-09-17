022e490abdc022e8b96a86346abf4183206f85ba
joaomarcelovalencachacon <joaomarcelovalencachacon@gmail.com>
2023-09-17 17:16:20 -0300

    feat(App): adjusted general layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import Styling from './styling/Background'
+import GeneralWindow from './components/GeneralWindow'
 
 function App() {
 
   return (
     <>
       <Styling>
-        <div>Texto aqui</div>
-          {/* <Navbar/> */}
-        {/* <Router>
-          <Routes>
-            <Route path="/colaborators" element={<Colaborators/>} />
-          </Routes>
-        </Router> */}
-        </Styling>
+        <div className="grid grid-cols-[1fr,7fr] gap-4">
+          <div className='mt-16'>
+            NavBar aqui
+          </div>
+          <div className='min-h-screen mt-16 mb-16 mr-12'>
+            <GeneralWindow>
+              Conteúdo da página aqui
+              {/* <Router>
+              <Routes>
+                <Route path="/colaborators" element={<Colaborators/>} />
+              </Routes>
+              </Router> */}
+            </GeneralWindow>
+          </div> 
+        </div> 
+      </Styling>
     </>
   )
 }
