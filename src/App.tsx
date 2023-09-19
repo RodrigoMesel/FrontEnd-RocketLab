e4fcb1149bdeb3aa2075444fe9e39f24bc5e600f
joaomarcelovalencachacon <joaomarcelovalencachacon@gmail.com>
2023-09-19 18:35:09 -0300

    add(App): added routers to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,29 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Styling from './styling/Background'
 import { NavBar } from './components/NavBar'
 import GeneralWindow from './components/GeneralWindow'
+import Colaborators from './pages/colaborators';
 
 function App() {
 
   return (
     <>
       <Styling>
-        
+      <Router>
         <div className="grid grid-cols-[1fr,7fr]  h-screen gap-4">
 
           <div className='mt-16 mb-16 ml-4 mr-2 h-1/2'>
             <NavBar/>
           </div>
-
           <div className=' mt-16 mb-16 mr-12'>
             <GeneralWindow>
-              Conteúdo da página aqui
-              {/* <Router>
               <Routes>
-                <Route path="/colaborators" element={<Colaborators/>} />
+                <Route path="/colaboradores" element={<Colaborators/>} />
               </Routes>
-              </Router> */}
             </GeneralWindow>
           </div> 
         </div> 
+        </Router>
       </Styling>
     </>
   )
