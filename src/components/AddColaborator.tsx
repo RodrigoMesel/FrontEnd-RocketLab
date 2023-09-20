a395b28b99709060e21fa6d4e2826cd602a6b263
Bonna Borsoi <bonnagilborsoi@gmail.com>
2023-09-20 16:51:08 -0300

    feat(AddColaborator: added AddColaborator button

---
+++ src/components/AddColaborator.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const AddColaborator: React.FC = () => {
+
+  
+  return (
+<div className='flex items-center justify-center'>
+    <button className='rounded-3xl bg-[#952323] h-12 w-12 flex items-center justify-center ml-1'>
+        <img src="./src/assets/add.svg" alt="Adicionar colaborador" />
+    </button>
+   
+</div>
+
+
+  );
+};
+
+export default AddColaborator;
