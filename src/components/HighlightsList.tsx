406739c51cfbcaa64edd4a716880bfa39cdc9d43
gabrielmpinha <gabriel.mp2000@gmail.com>
2023-09-26 19:00:12 -0300

    feat: added react-modal dependency + modal on dash

--- src/components/HighlightsList.tsx
+++ src/components/HighlightsList.tsx
@@ -1,13 +1,19 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
+import HighlightsModal from "./HighlightsModal";
+import { HighlightsModalContext } from "./context/HighlightsModalContext";
 
 const HighlightsList: React.FC = () => {
-  const [data, setData] = useState({
-    goal: [{ grade: 0, id: 0, name: "", role: "" }],
-    superGoal: [{ grade: 0, id: 0, name: "", role: "" }],
-    challenge: [{ grade: 0, id: 0, name: "", role: "" }],
-    nothing: [{ grade: 0, id: 0, name: "", role: "" }],
-  });
+  const highlightsContext = useContext(HighlightsModalContext);
+
+  interface dataI {
+    goal: [{ grade: number; id: number; name: string; role: string }];
+    superGoal: [{ grade: number; id: number; name: string; role: string }];
+    challenge: [{ grade: number; id: number; name: string; role: string }];
+    nothing: [{ grade: number; id: number; name: string; role: string }];
+  }
+
+  const [data, setData] = useState<dataI>();
   useEffect(() => {
     axios
       .get("http://localhost:3000/colaborator-indicator/statistics")
@@ -33,90 +39,103 @@ const HighlightsList: React.FC = () => {
     <>
       <ul className="max-w-md divide-y divide-gray-200">
         <li className="pb-3">
-          <a href="#" className="block h-full w-full no-underline">
-            <div className="flex items-center space-x-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="33"
-                height="33"
-                viewBox="0 0 33 33"
-                fill="none"
-              >
-                <circle cx="16.5" cy="16.1465" r="16" fill="#6186D3" />
-              </svg>
-              <p>
-                <span className="font-bold">
-                  {data.challenge.length} colaboradores{" "}
-                </span>
-                atingiram o Desafio de seu Indicador!
-              </p>
-            </div>
-          </a>
+          <div
+            className="flex items-center space-x-4 cursor-pointer"
+            onClick={() =>
+              highlightsContext.handleOpen({ colaboratorList: data?.challenge })
+            }
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="33"
+              height="33"
+              viewBox="0 0 33 33"
+              fill="none"
+            >
+              <circle cx="16.5" cy="16.1465" r="16" fill="#6186D3" />
+            </svg>
+            <p>
+              <span className="font-bold">
+                {data ? data.challenge.length : 0} colaboradores{" "}
+              </span>
+              atingiram o Desafio de seu Indicador!
+            </p>
+          </div>
         </li>
         <li className="py-3">
-          <a href="#" className="block h-full w-full no-underline">
-            <div className="flex items-center space-x-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="33"
-                height="33"
-                viewBox="0 0 33 33"
-                fill="none"
-              >
-                <circle cx="16.5" cy="16.293" r="16" fill="#32B97C" />
-              </svg>
-              <p>
-                <span className="font-bold">
-                  {data.superGoal.length} colaboradores{" "}
-                </span>
-                atingiram a Supermeta de seu Indicador!
-              </p>
-            </div>
-          </a>
+          <div
+            className="flex items-center space-x-4 cursor-pointer"
+            onClick={() =>
+              highlightsContext.handleOpen({ colaboratorList: data?.superGoal })
+            }
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="33"
+              height="33"
+              viewBox="0 0 33 33"
+              fill="none"
+            >
+              <circle cx="16.5" cy="16.293" r="16" fill="#32B97C" />
+            </svg>
+            <p>
+              <span className="font-bold">
+                {data ? data.superGoal.length : 0} colaboradores{" "}
+              </span>
+              atingiram a Supermeta de seu Indicador!
+            </p>
+          </div>
         </li>
         <li className="py-3">
-          <a href="#" className="block h-full w-full no-underline">
-            <div className="flex items-center space-x-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="33"
-                height="28"
-                viewBox="0 0 33 28"
-                fill="none"
-              >
-                <circle cx="16.5" cy="16.4395" r="16" fill="#AC72C1" />
-              </svg>
-              <p>
-                <span className="font-bold">
-                  {data.goal.length} colaboradores{" "}
-                </span>
-                atingiram a Meta de seu Indicador!
-              </p>
-            </div>
-          </a>
+          <div
+            className="flex items-center space-x-4 cursor-pointer"
+            onClick={() =>
+              highlightsContext.handleOpen({ colaboratorList: data?.goal })
+            }
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="33"
+              height="28"
+              viewBox="0 0 33 28"
+              fill="none"
+            >
+              <circle cx="16.5" cy="16.4395" r="16" fill="#AC72C1" />
+            </svg>
+            <p>
+              <span className="font-bold">
+                {data ? data.goal.length : 0} colaboradores{" "}
+              </span>
+              atingiram a Meta de seu Indicador!
+            </p>
+          </div>
         </li>
         <li className="pt-3">
-          <a href="#" className="block h-full w-full no-underline">
-            <div className="flex items-center space-x-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="33"
-                height="28"
-                viewBox="0 0 33 28"
-                fill="none"
-              >
-                <circle cx="16.5" cy="16.4395" r="16" fill="#F16062" />
-              </svg>
-              <p>
-                <span className="font-bold">
-                  {data.nothing.length} colaboradores{" "}
-                </span>
-                não atingiram seu Indicador.
-              </p>
-            </div>
-          </a>
+          <div
+            className="flex items-center space-x-4 cursor-pointer"
+            onClick={() =>
+              highlightsContext.handleOpen({ colaboratorList: data?.nothing })
+            }
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="33"
+              height="28"
+              viewBox="0 0 33 28"
+              fill="none"
+            >
+              <circle cx="16.5" cy="16.4395" r="16" fill="#F16062" />
+            </svg>
+            <p>
+              <span className="font-bold">
+                {data ? data.nothing.length : 0} colaboradores{" "}
+              </span>
+              não atingiram seu Indicador.
+            </p>
+          </div>
         </li>
       </ul>
+      <HighlightsModal />
     </>
   );
 };
