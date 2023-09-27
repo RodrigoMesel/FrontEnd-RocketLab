96e03cab936559a7fc3036672bcf61d4b783e4d4
Bonna Borsoi <bonnagilborsoi@gmail.com>
2023-09-27 18:37:50 -0300

    feat(Colaborator): added new components

--- src/pages/Colaborator.tsx
+++ src/pages/Colaborator.tsx
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import SearchBar from '../components/SearchBar'
+import { BrowserRouter as Router, Route, useParams } from 'react-router-dom';
 import { getUserData } from '../utils/getUserData'
 import ColaboratorImage from '../components/ColaboratorImage';
+import IndicatorCard from '../components/IndicatorCard';
+import IndicatorCardteste from '../components/IndicatorCardteste';
+import { getIndicatorName } from '../utils/getIndicatorName';
+import { getMonthStatistics } from '../utils/getMonthStatistics'; 
 
 type UserData = {
     id: number;
@@ -9,16 +14,39 @@ type UserData = {
     grade: number;
     role: string;
   };
+  
+type MonthStatistics = {
+    goal: number;
+    superGoal: number;
+    challenge: number;
+    monthIndicators: Array<{
+      id: number;
+      weight: number;
+      goal: number;
+      superGoal: number;
+      challenge: number;
+  
+    }>;
+};
 
 export default function Colaborator() {
-  const [userData, setUserData] = useState<UserData>({ id: 0, name: "", grade: 0, role: "" });
+    const [userData, setUserData] = useState<UserData>({ id: 0, name: "", grade: 0, role: "" });
+    
+    const [monthStats, loading] = getMonthStatistics(2, 1); 
+    const [indicatorName, setIndicatorName] = useState<string | null>(null);
 
-  // No momento, estamos passando um id fixo! Precisa alterar isso depois
-  const data = getUserData(2);
+    // No momento, estamos passando um id fixo! Precisa alterar isso depois
+    const data = getUserData(2);
+    const nameData = getIndicatorName(99);
 
-  useEffect(() => {
-    setUserData(data);
-  }, [data]);
+    useEffect(() => {
+        setIndicatorName(nameData);
+    }, [nameData]);
+
+
+    useEffect(() => {
+        setUserData(data);
+    }, [data]);
 
   return (
     <>
@@ -26,32 +54,44 @@ export default function Colaborator() {
         <SearchBar />
       </div>
       
-      <div className='flex flex-col  flex-column space-x-2 ml-5 mt-8 mr-4'>
+      <div className='flex flex-row space-x-5 ml-5 mt-8 mr-4'>
         {/* {userData.id !== 0 ? (
           <> */}
-          <ColaboratorImage/>
-          <div className='flex flex-row  flex-column space-x-2 ml-5 mt-8 mr-4'>
-            <div className='font-bold text-3xl'>{userData.name}</div>
+
+        <ColaboratorImage/>
+        <div className='flex flex-col'>
             <div>{userData.role}</div>
+            <div className='font-bold text-3xl'>{userData.name}</div>
+        </div>
 
 
+        <div>
             <div className='flex items-center space-x-1 rounded-xl bg-[#6186D3] h-8 w-16 text-center text-white font-bold'>
                 <img src="./src/assets/grade.svg" className='ml-4'/>
                 <div className='mr-4'>{userData.grade}</div>
             </div>
         </div>
+
    
           {/* </>
         ) : (
           <div>Carregando...</div>
         )} */}
         </div>
-        
-      <div className='flex'>
-        <div className='flex flex-row justify-items-start px-10 py-10' style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
-        </div>
-        <div className="grow h-14 ..."></div>
-      </div>
+
+        <div className='flex flex-col space-y-2'>
+                {monthStats && monthStats.monthIndicators.map((indicator) => (
+        <IndicatorCard
+            key={indicator.id}
+            name="Converter 20 novos clientes"
+            weight={indicator.weight}
+            goal={indicator.goal}
+            supergoal={indicator.superGoal}
+            challenge={indicator.challenge}
+        />
+        ))}
+
+        </div>  
     </>
   )
 }
