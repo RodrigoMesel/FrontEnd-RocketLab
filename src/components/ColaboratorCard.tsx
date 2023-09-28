d0ba62a626f62f4272e64e37ca57c7b6cf3fea18
gabrielmpinha <gabriel.mp2000@gmail.com>
2023-09-28 17:13:09 -0300

    feat: created DashboardGrade to dinamically change grade color

--- src/components/ColaboratorCard.tsx
+++ src/components/ColaboratorCard.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import ColaboratorImage from './ColaboratorImage';
+import React from "react";
+import ColaboratorImage from "./ColaboratorImage";
+import ColaboratorGrade from "./ColaboratorGrade";
 
 const ColaboratorCard: React.FC = () => {
-
-  
   return (
     <>
-        <div className='flex flex-col rounded-2xl bg-[#FBFBFB] h-44 w-40 text-center items-center text-[#312843]'>
-            <div className='flex items-center mt-5'>
-                <ColaboratorImage/>
-            </div>
-                <div className='font-bold mt-1'>Alice Martins</div>
-                <div className=''>Marketing</div>
-
-
-            <div className='flex items-center space-x-1 rounded-xl bg-[#6186D3] h-8 w-16 text-center text-white font-bold'>
-                <img src="./src/assets/grade.svg" className='ml-4'/>
-                <div className='mr-4'>9</div>
-            </div>
+      <div className="flex flex-col rounded-2xl bg-[#FBFBFB] h-44 w-40 text-center items-center text-[#312843]">
+        <div className="flex items-center mt-5">
+          <ColaboratorImage />
         </div>
-    </>
+        <div className="font-bold mt-1">Alice Martins</div>
+        <div className="">Marketing</div>
 
+        <ColaboratorGrade grade={5} />
+      </div>
+    </>
   );
 };
 
