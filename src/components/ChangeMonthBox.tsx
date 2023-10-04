e260ef918493ecfaaeec1c018210071afdc13f64
Bonna Borsoi <bonnagilborsoi@gmail.com>
2023-10-03 21:55:47 -0300

    style(ChangeMonthBox): adjusted left and right buttons

--- src/components/ChangeMonthBox.tsx
+++ src/components/ChangeMonthBox.tsx
@@ -1,16 +1,36 @@
-import {getMonthName} from '../utils/getMonthName'
+import { getMonthName } from '../utils/getMonthName';
+import React from 'react';
+import left from '../assets/left.svg';
+import right from '../assets/right.svg';
 
-interface StatsTextBoxProps {
+interface ChangeMonthBoxProps {
   monthNumber: number;
+  incrementNumber: () => void; 
+  decrementNumber: () => void; 
 }
 
-const StatsTextBox: React.FC<StatsTextBoxProps> = ({ monthNumber }) => {
+const ChangeMonthBox: React.FC<ChangeMonthBoxProps> = ({
+  monthNumber,
+  incrementNumber,
+  decrementNumber,
+}) => {
   return (
     <>
-      <div className="rounded-xl border border-solid w-fit px-2.5 py-1 border-[#D9D9D9]">
-        <p className="font-medium text-base text-[#A3A3A3]">{getMonthName(monthNumber)}</p>
+      <div className="flex flex-row rounded-xl border border-solid w-fit px-2.5 py-1 border-[#D9D9D9] space-x-1">
+        <button onClick={decrementNumber}>
+          <img src={left} alt="" className="h-3 w-3" />
+        </button>
+
+        <p className="font-medium text-base text-[#A3A3A3]">
+          {getMonthName(monthNumber)}
+        </p>
+
+        <button onClick={incrementNumber}>
+          <img src={right} alt="" className="h-3 w-3" />
+        </button>
       </div>
     </>
   );
 };
-export default StatsTextBox;
+
+export default ChangeMonthBox;
