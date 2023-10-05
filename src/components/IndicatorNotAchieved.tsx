0cf773e650e10f7291555e2a077583167ef37043
Rodrigo Mesel <romesel1@gmail.com>
2023-10-04 23:00:19 -0300

    feat: indicators not achieved percentual fix

--- src/components/IndicatorNotAchieved.tsx
+++ src/components/IndicatorNotAchieved.tsx
@@ -18,22 +18,26 @@ interface IndicatorNotAchieveProps{
 }
 
 const IndicatorNotAchieve: React.FC<IndicatorNotAchieveProps> = ({nothingIndicators} : IndicatorNotAchieveProps) => {
-
+  console.log(nothingIndicators)
   return (
-    <div className='rounded-lg border border-solid py-8 px-3 flex flex-col max-w-xs h-[26.5rem] gap-8 overflow-scroll'>
+    <div className='rounded-lg border border-solid py-8 px-3 flex flex-col max-w-xs gap-8 overflow-scroll'>
 
       <span className='text-2xl'>Indicadores <span className='font-bold'>não alcançados</span> neste mês</span>
 
-      {nothingIndicators!.map((indicator, index) => (
+      {nothingIndicators.length > 0 && nothingIndicators!.map((indicator, index) => (
                     <div key={index} className='flex flex-row justify-between items-center px-3 gap-5'>
                         <div className='flex flex-row max-w-sm items-center gap-2'>
                           <span className='rounded-xl bg-[#F16062] w-6 h-3'></span>
-                          <span>{indicator.name} ({indicator.goal}):</span>
+                          <span>{indicator.name} ({indicator.goal}{indicator.unity === "Percentual" &&"%"}):</span>
                         </div>
 
-                        <span className='font-bold'>{indicator.result}</span>
+                        <span className='font-bold'>{indicator.result}{indicator.unity === "Percentual" &&"%"}</span>
                     </div>
                   ))}
+
+      {nothingIndicators.length == 0 && 
+            <span className='text-[#A3A3A3] text-xl text-center mt-10 flex'>Todos os indicadores foram alcançados!</span>
+      }
 
     </div>
 
